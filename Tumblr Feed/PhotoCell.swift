75ebc3818449272f09df12cf3f99f004d4b99928
Sudheesh Bhattarai <sudheeshbhattarai@Sudheeshs-MacBook-Pro.local>
2017-02-10 21:33:18 -0500

    Adding working code for Tumblr App.

---
+++ Tumblr Feed/PhotoCell.swift
@@ -0,0 +1,26 @@
+//
+//  PhotoCell.swift
+//  Tumblr Feed
+//
+//  Created by Sudheesh Bhattarai on 2/2/17.
+//  Copyright © 2017 Sudheesh Bhattarai. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+    
+     @IBOutlet weak var photoView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
